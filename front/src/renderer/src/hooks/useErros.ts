d207aad17f1331ad5d537341d9801b9df56acd27
odanielchristopher] <o.dani.br@gmail.com>
2024-11-27 13:02:59 -0300

    feat: criando o hook useErrors

---
+++ front/src/renderer/src/hooks/useErros.ts
@@ -0,0 +1,40 @@
+import { useCallback, useState } from 'react';
+
+interface ErrorProps {
+  field: string
+  message: string
+}
+
+export default function useErrors() {
+  const [errors, setErrors] = useState<ErrorProps[]>([]);
+
+  const setError = useCallback(({ field, message }) => {
+    const errorAlreadyExists = errors.find((error) => error.field === field);
+
+    if (errorAlreadyExists) {
+      return;
+    }
+
+    setErrors((prevState) => [
+      ...prevState,
+      { field, message },
+    ]);
+  }, [errors]);
+
+  const removeError = useCallback((fieldName) => {
+    setErrors((prevState) => prevState.filter(
+      (error) => error.field !== fieldName,
+    ));
+  }, []);
+
+  const getErrorMessageByFieldName = useCallback((fieldName) => (
+    errors.find((error) => error.field === fieldName)?.message
+  ), [errors]);
+
+  return {
+    errors,
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+  };
+}
