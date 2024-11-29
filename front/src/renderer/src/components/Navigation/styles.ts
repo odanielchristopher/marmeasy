9fd33da1f828a380d34e0b40d7abbdf9626ee9fb
odanielchristopher] <o.dani.br@gmail.com>
2024-11-29 12:12:55 -0300

    feat (front): finalizando o componente Navigation

--- front/src/renderer/src/components/Navigation/styles.ts
+++ front/src/renderer/src/components/Navigation/styles.ts
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ILinkNavigation {
+  active?: boolean
+}
 
 export const Container = styled.div`
   align-items: center;
@@ -9,6 +13,67 @@ export const Container = styled.div`
   flex-direction: column;
   height: calc(100vh - 4.8rem);
   margin: 2.4rem auto;
-  padding: 1.0rem;
+  padding: 1.6rem 1rem;
   width: min-content;
+`;
+
+export const LogoContainer = styled.div`
+  height: 5.6rem;
+  margin-bottom: 6.4rem;
+  width: 5.6rem;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 80%;
+`;
+
+export const FootContainer = styled.div`
+  justify-self: flex-end;
+  align-self: center;
+`;
+
+export const LinkNavigation = styled.button<ILinkNavigation>`
+  align-items: center;
+  background: transparent;
+  border: none;
+  color: ${({ theme }) => theme.colors.black.main};
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+  transition: all ease-in 100ms;
+
+  span {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+
+  & + & {
+    margin-top: 3.2rem;
+  }
+
+  &:hover {
+    color: #fff;
+
+    span {
+      text-decoration: underline;
+    }
+  }
+
+  ${({ active }) =>
+    active &&
+    css`
+      color: #fff;
+
+      span {
+        text-decoration: underline;
+      }
+    `}
 `;
