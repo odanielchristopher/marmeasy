7dbab857e8a69501d36a750997eaaa5745f6922d
odanielchristopher] <o.dani.br@gmail.com>
2024-12-06 20:04:33 -0300

    refactor (back): colocando as logica do express em uma pasta server

---
+++ back/src/server/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeSignInController } from '../factories/makeSignInController';
+import { makeSignUpController } from '../factories/makeSignUpController';
+import { makeEditAccountController } from '../factories/mekeEditAccountController';
+
+import { middlewareAdapter } from './adapters/middlewareAdapter';
+import { routeAdapter } from './adapters/routeAdapter';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/signUp', routeAdapter(makeSignUpController()));
+app.post('/signIn', routeAdapter(makeSignInController()));
+app.get('/editAccount',
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeEditAccountController())
+);
+
+app.listen(3001, () => console.log('Server started at http://localhost:3001'));
