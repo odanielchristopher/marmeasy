2a763327dd579a9a8fb7d5dd33ce266471f59062
odanielchristopher] <o.dani.br@gmail.com>
2024-12-06 20:18:30 -0300

    refactor (back): renomeando a página db -> database

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,13 +1,1 @@
-import express from 'express';
-import cors from 'cors';
-import router from './http/routes/router';
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.use(router);
-
- 
-app.listen(3333, () => console.log('server running on port 3333'));
+import './server';
