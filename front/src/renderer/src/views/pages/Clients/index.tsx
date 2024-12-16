4eba8af22afee29f9c94ac04b1cc4b669c2f51be
odanielchristopher] <o.dani.br@gmail.com>
2024-12-16 17:06:39 -0300

    feat (front): implementando o inputSearch

--- front/src/renderer/src/views/pages/Clients/index.tsx
+++ front/src/renderer/src/views/pages/Clients/index.tsx
@@ -1,15 +1,22 @@
 import { TbUsers } from 'react-icons/tb';
 
 import Fab from '@renderer/views/components/Fab';
+import { IoIosSearch } from 'react-icons/io';
 
 import { clientsService } from '@renderer/app/services/clientsService';
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
-import { Container, Content, Header, NotFoundContainer } from './styles';
+import { useEffect, useMemo, useState } from 'react';
+import {
+  Container,
+  Content,
+  Header,
+  InputContainer,
+  NotFoundContainer,
+  SearchInput,
+} from './styles';
 
 import { Client } from '@renderer/app/entities/Client';
 import formatPhone from '@renderer/app/utils/formatPhone';
-import Select from '@renderer/views/components/Select';
 import CardList from './components/CardList';
 
 import notFoundImage from '@renderer/assets/Images/NotFound.svg';
@@ -17,6 +24,7 @@ import Loader from '@renderer/views/components/Loader';
 
 export default function Clients() {
   const [clients, setClients] = useState<Client[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const { data, isLoading } = useQuery({
     queryKey: ['clients', 'getAll'],
@@ -29,6 +37,18 @@ export default function Clients() {
   function loadClient() {
     setClients(data ?? []);
   }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredClients = useMemo(
+    () =>
+      clients.filter((contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase())),
+    [clients, searchTerm],
+  );
+  const isSearchEmpty = filteredClients.length < 1;
+  const hasClient = filteredClients.length > 0;
 
   useEffect(() => {
     loadClient();
@@ -45,33 +65,42 @@ export default function Clients() {
         <p>Gerencie os clientes do seu estabelecimento</p>
       </Header>
 
-      <Select
-        placeholder="Cliente"
-        options={clients.map((clients) => ({
-          label: clients.name,
-          value: clients.id,
-        }))}
-      />
+      <InputContainer>
+        <IoIosSearch size={28} />
+        <SearchInput
+          placeholder="Pesquisar por nome..."
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+        />
+      </InputContainer>
 
       {isLoading && <Loader $isLoading size={50} />}
-      <Content>
-        {!!clients && (
-          <NotFoundContainer>
-            <img src={notFoundImage} alt="Clientes não encontrados" />
-            <p>Não encontramos nenhum cliente!</p>
-          </NotFoundContainer>
-        )}
-        <CardList
-          cards={clients.map((client) => ({
-            id: client.id,
-            name: client.name,
-            address: client.address,
-            phone: formatPhone(client.phone ?? ''),
-            balance: client.balance as number,
-            ordersCount: 12,
-          }))}
-        />
-      </Content>
+
+      {!isLoading && (
+        <Content>
+
+          {isSearchEmpty && (
+            <NotFoundContainer>
+              <img src={notFoundImage} alt="Clientes não encontrados" />
+              <p>Não encontramos nenhum cliente!</p>
+            </NotFoundContainer>
+          )}
+
+
+          {hasClient && (
+            <CardList
+              cards={filteredClients.map((client) => ({
+                id: client.id,
+                name: client.name,
+                address: client.address,
+                phone: formatPhone(client.phone ?? ''),
+                balance: client.balance as number,
+                ordersCount: 12,
+              }))}
+            />
+          )}
+        </Content>
+      )}
     </Container>
   );
 }
--- front/src/renderer/src/views/pages/Clients/styles.ts
+++ front/src/renderer/src/views/pages/Clients/styles.ts
@@ -56,7 +56,31 @@ export const NotFoundContainer = styled.div`
       display: block;
     }
   }
+`;
 
+export const InputContainer = styled.div`
+  position: relative;
+
+  svg {
+    color: ${({ theme }) => theme.colors.gray.light};
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 1.0rem;
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  max-width: 30.0rem;
+  padding: .8rem 1.0rem;
+  padding-left: 4.2rem;
+  border-radius: 1.0rem;
+  height: 4.8rem;
+  border: .08rem solid ${({ theme }) => theme.colors.gray.lighter};
+  font-size: 1.6rem;
+  line-height: 1.8rem;
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.08);
 `;
 
 
