e80ec4b5630739b1e1feaf9950e9a9438cf79b29
odanielchristopher] <o.dani.br@gmail.com>
2025-01-23 23:11:22 -0300

    feat: criando as queries

--- front/src/renderer/src/app/services/ordersService/getAllByClientId.ts
+++ front/src/renderer/src/app/services/ordersService/getAllByClientId.ts
@@ -1,14 +1,12 @@
 import { Order } from '@renderer/app/entities/Order';
 import { httpClient } from '../httpClient';
 
-export type GetAllOrdersResponse = Array<Order>;
-
 export interface GetAllOrdersParams {
   clientId: string;
 }
 
 export async function getAllByClientId({ clientId }: GetAllOrdersParams) {
-  const { data } = await httpClient.get<GetAllOrdersResponse>('/orders', {
+  const { data } = await httpClient.get<Order[]>('/orders', {
     params: { clientId },
   });
 
--- front/src/renderer/src/app/services/ordersService/index.ts
+++ front/src/renderer/src/app/services/ordersService/index.ts
@@ -1,7 +1,11 @@
 import { createByClientId } from './createByClientId';
 import { getAllByClientId } from './getAllByClientId';
+import { removeByClientId } from './removeByClientId';
+import { updateByClientId } from './updateByClientId';
 
 export const ordersService = {
   getAllByClientId,
   createByClientId,
+  removeByClientId,
+  updateByClientId,
 };
