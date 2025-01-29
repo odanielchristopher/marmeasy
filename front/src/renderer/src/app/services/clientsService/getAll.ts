4fc909c36d7cc4c2b6f7124a25f12e5c9dfa3b8f
odanielchristopher] <o.dani.br@gmail.com>
2025-01-29 10:19:46 -0300

    feat: iniciando a paginacao

--- front/src/renderer/src/app/services/clientsService/getAll.ts
+++ front/src/renderer/src/app/services/clientsService/getAll.ts
@@ -1,10 +1,19 @@
 import { Client } from '@renderer/app/entities/Client';
-import { httpClient } from '../httpClient';
+import axios from 'axios';
 
 export type GetAllResponse = Array<Client>;
 
+const httpClient = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
 export async function getAll() {
-  const { data } = await httpClient.get<GetAllResponse>('/clients');
+  const { data } = await httpClient.get<GetAllResponse>('/clients', {
+    params: {
+      _page: 1,
+      _per_page: 10,
+    },
+  });
 
   return data;
 }
