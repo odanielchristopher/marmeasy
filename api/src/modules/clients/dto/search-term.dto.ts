0f4b6627403124ce9e037ac00333b60cf1980d09
odanielchristopher] <o.dani.br@gmail.com>
2025-01-29 21:56:54 -0300

    feat: criacao do searchTermDto

---
+++ api/src/modules/clients/dto/search-term.dto.ts
@@ -0,0 +1,7 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class SearchTermDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O termo de pesquisa é obrigatório.' })
+  query: string;
+}
