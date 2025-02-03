879b06200f88e04098fde61a9c3bda3b9ab196a4
odanielchristopher] <o.dani.br@gmail.com>
2025-02-03 20:35:03 -0300

    perf: economizando renders da sessao de pagamentos

--- front/src/renderer/src/views/layouts/PageLayout/components/Aside/Payments/usePayments.ts
+++ front/src/renderer/src/views/layouts/PageLayout/components/Aside/Payments/usePayments.ts
@@ -1,41 +1,43 @@
 import { Client } from '@renderer/app/entities/Client';
 import { Payment } from '@renderer/app/entities/Payment';
 import { usePaymentsQuery } from '@renderer/app/hooks/queries/usePaymetsQuery';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 export default function usePayments(client: Client | null) {
   const [isOpenNewPaymentModal, setIsOpenNewPaymentModal] = useState(false);
   const [selectedPayment, setSelectedPayment] = useState<Payment | null>(null);
   const [isOpenEditPaymentModal, setIsOpenEditPaymentModal] = useState(false);
 
-  function handleSelectedPayment(payment: Payment) {
+  const handleSelectedPayment = useCallback((payment: Payment) => {
     setSelectedPayment(payment);
     handleOpenEditPaymentModal();
-  }
+  }, []);
 
-  function handleOpenEditPaymentModal() {
+  const handleOpenEditPaymentModal = useCallback(() => {
     setIsOpenEditPaymentModal(true);
-  }
+  }, []);
 
-  function handleCloseEditPaymentModal() {
+  const handleCloseEditPaymentModal = useCallback(() => {
     setIsOpenEditPaymentModal(false);
-  }
+  }, []);
 
-  function handleOpenNewPaymentModal() {
+  const handleOpenNewPaymentModal = useCallback(() => {
     setIsOpenNewPaymentModal(true);
-  }
+  }, []);
 
-  function handleCloseNewPaymentModal() {
+  const handleCloseNewPaymentModal = useCallback(() => {
     setIsOpenNewPaymentModal(false);
-  }
+  }, []);
 
   const { isLoading, payments } = usePaymentsQuery({ clientId: client!.id });
 
   const hasPayments = payments.length > 0;
 
-  const sortedPayments = payments.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
-  );
+  const sortedPayments = useMemo(() => {
+    return payments.sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+    );
+  }, [payments]);
 
   return {
     payments: sortedPayments,
