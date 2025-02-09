6d1131851af32ec2de628edbdaadaf5a43735018
odanielchristopher] <o.dani.br@gmail.com>
2025-02-09 00:26:33 -0300

    feat (back): finalizando o modulo de expenses

--- api/src/shared/database/repositories/expenses.repository.ts
+++ api/src/shared/database/repositories/expenses.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Expense } from 'src/modules/expenses/entities/expense.entity';
+import { Expense as PrismaExpense } from '@prisma/client';
+
 import {
   CreateExpenseDto,
   DeleteExpenseDto,
@@ -7,22 +8,68 @@ import {
   IExpensesRepository,
   UpdateExpenseDto,
 } from '../interfaces/expenses-repository.interface';
+
+import { Expense } from 'src/modules/expenses/entities/expense.entity';
+import { ExpenseMapper } from '../mappers/expense.mapper';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ExpensesRepository implements IExpensesRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  findOneByUserId(findOneDto: FindOneExpenseDto): Promise<Expense> {
-    throw new Error('Method not implemented.');
+  async findOneByUserId(findOneDto: FindOneExpenseDto): Promise<Expense> {
+    const { id, userId } = findOneDto;
+
+    const findedExpense = await this.prismaService.expense.findUnique({
+      where: { id, userId },
+    });
+
+    return this.parser(findedExpense);
   }
-  create(createDto: CreateExpenseDto): Promise<Expense> {
-    throw new Error('Method not implemented.');
+
+  async create(createDto: CreateExpenseDto): Promise<Expense> {
+    const { userId, data } = createDto;
+
+    const { date, type, value } = data;
+
+    const createdExpense = await this.prismaService.expense.create({
+      data: {
+        userId,
+        date,
+        type,
+        value,
+      },
+    });
+
+    return this.parser(createdExpense);
   }
-  update(updateDto: UpdateExpenseDto): Promise<Expense> {
-    throw new Error('Method not implemented.');
+
+  async update(updateDto: UpdateExpenseDto): Promise<Expense> {
+    const { userId, data } = updateDto;
+
+    const { id, date, type, value } = data;
+
+    const updatedExpense = await this.prismaService.expense.update({
+      where: { userId, id },
+      data: {
+        date,
+        type,
+        value,
+      },
+    });
+
+    return this.parser(updatedExpense);
   }
-  delete(deleteDto: DeleteExpenseDto): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async delete(deleteDto: DeleteExpenseDto): Promise<void> {
+    const { id, userId } = deleteDto;
+
+    await this.prismaService.expense.delete({
+      where: { userId, id },
+    });
+  }
+
+  private parser(prismaExpense: PrismaExpense) {
+    return ExpenseMapper.getInstance().toDomain(prismaExpense);
   }
 }
