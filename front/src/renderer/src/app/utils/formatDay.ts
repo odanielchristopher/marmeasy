d09d8fee9c2b987aca586e0d28b4d92c94cc1aef
odanielchristopher] <o.dani.br@gmail.com>
2025-02-11 12:21:23 -0300

    lint: formatando o codigo corretamente

--- front/src/renderer/src/app/utils/formatDay.ts
+++ front/src/renderer/src/app/utils/formatDay.ts
@@ -3,5 +3,6 @@ import { ptBR } from 'date-fns/locale';
 
 export function formatDay(day: string, monthYear: string) {
   const date = parse(`${monthYear}-${day}`, 'yyyy-MM-dd', new Date());
-  return format(date, 'EEEE, d \'de\' MMM. yyyy', { locale: ptBR });
+  // eslint-disable-next-line quotes
+  return format(date, "EEEE, d 'de' MMM. yyyy", { locale: ptBR });
 }
