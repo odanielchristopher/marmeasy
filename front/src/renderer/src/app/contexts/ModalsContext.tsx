948aa6d005aaaede0225c543e47e28a925608a77
odanielchristopher] <o.dani.br@gmail.com>
2025-02-19 10:40:21 -0300

    feat (front): retirando a funcao de apagar conta

--- front/src/renderer/src/app/contexts/ModalsContext.tsx
+++ front/src/renderer/src/app/contexts/ModalsContext.tsx
@@ -2,26 +2,14 @@ import React, { createContext, useCallback, useState } from 'react';
 
 export interface ModalsContextValue {
   isProfileModalOpen: boolean;
-  isDeleteUserModalOpen: boolean;
   handleOpenProfileModal(): void;
   handleCloseProfileModal(): void;
-  handleOpenDeleteUserModal(): void;
-  handleCloseDeleteUserModal(): void;
 }
 
 export const ModalsContext = createContext({} as ModalsContextValue);
 
 export function ModalsProvider({ children }: { children: React.ReactNode }) {
   const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
-  const [isDeleteUserModalOpen, setDeleteUserModalOpen] = useState(false);
-
-  const handleCloseDeleteUserModal = useCallback(() => {
-    setDeleteUserModalOpen(false);
-  }, []);
-
-  const handleOpenDeleteUserModal = useCallback(() => {
-    setDeleteUserModalOpen(true);
-  }, []);
 
   const handleOpenProfileModal = useCallback(() => {
     setIsProfileModalOpen(true);
@@ -35,9 +23,6 @@ export function ModalsProvider({ children }: { children: React.ReactNode }) {
     <ModalsContext.Provider
       value={{
         isProfileModalOpen,
-        isDeleteUserModalOpen,
-        handleCloseDeleteUserModal,
-        handleOpenDeleteUserModal,
         handleOpenProfileModal,
         handleCloseProfileModal,
       }}
