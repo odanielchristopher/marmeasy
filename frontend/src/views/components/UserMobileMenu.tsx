949968daf1fa30c1e707ecace03f11f9a7364688
daniel christopher <daniel.2005.christopher@gmail.com>
2025-04-10 00:41:47 -0300

    refact: change dropdown's border color

--- frontend/src/views/components/UserMobileMenu.tsx
+++ frontend/src/views/components/UserMobileMenu.tsx
@@ -54,7 +54,7 @@ export function UserMobileMenu({ currentPathname }: IUserMobileMenuProps) {
           'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
           'duration-300',
           'shadow-default',
-          'border-b-2 border-transparent dark:border-accent dark:bg-background',
+          'border-b dark:bg-background',
         )}
       >
         <DropdownMenuGroup className="space-y-3">
