42615d01058dc21bee5a338aeddd7986d3c8d3df
daniel christopher <daniel.2005.christopher@gmail.com>
2025-04-09 21:26:36 -0300

    feat: create component AppNavigation

--- frontend/src/views/components/AppHeader.tsx
+++ frontend/src/views/components/AppHeader.tsx
@@ -1,33 +1,32 @@
-import { Link } from 'react-router';
+import { useLocation } from 'react-router';
 
-import { routes } from '@app/Router/routes';
+import { useIsMobile } from '@app/hooks/useIsMobile';
 import { Logo } from '@views/assets/Logo';
 
+import { AppNavigation } from './AppNavigation';
 import { ThemeSwitcher } from './ThemeSwitcher';
+import { UserMenu } from './UserMenu';
+import { UserMobileMenu } from './UserMobileMenu';
 
 export function AppHeader() {
-  return (
-    <header className="px-6 py-4 flex border-b mb-4 justify-between bg-primary-400">
-      <div className="flex gap-2 items-center">
-        <div>
-          <Logo />
-        </div>
+  const { pathname: currentPathname } = useLocation();
+  const isMobile = useIsMobile();
 
-        <Link
-          className="text-xl font-medium hover:text-gray-200 hover:underline"
-          to={routes.index}
-        >
-          Home
-        </Link>
-        <Link
-          className="text-xl font-medium hover:text-gray-200 hover:underline"
-          to={routes.users.new.row}
-        >
-          Create user
-        </Link>
+  return (
+    <header className="fixed z-10 top-0 w-full px-6 py-4 f flex border-b mb-4 items-center justify-between bg-primary-400 dark:bg-background">
+      <div className="flex gap-2 items-center">
+        <Logo className="text-teal-950 dark:text-foreground" />
+
+        <AppNavigation currentPathname={currentPathname} />
       </div>
 
-      <ThemeSwitcher />
+      <div className="flex flex-1 justify-end items-center gap-2">
+        <ThemeSwitcher className="bg-transparent text-white hover:!bg-[#DEE2E6]/20  hover:text-white" />
+
+        {isMobile && <UserMobileMenu currentPathname={currentPathname} />}
+
+        {!isMobile && <UserMenu />}
+      </div>
     </header>
   );
 }
---
+++ frontend/src/views/components/AppNavigation.tsx
@@ -0,0 +1,43 @@
+import { BookText, Users } from 'lucide-react';
+import { Link } from 'react-router';
+
+import { cn } from '@app/lib/utils';
+import { routes } from '@app/Router/routes';
+
+interface IAppNavigationProps {
+  currentPathname: string;
+}
+
+export const navItems = [
+  {
+    label: 'Clientes',
+    pathname: routes.index,
+    icon: Users,
+  },
+  {
+    label: 'Formulário',
+    pathname: routes.form,
+    icon: BookText,
+  },
+] as const;
+
+export function AppNavigation({ currentPathname }: IAppNavigationProps) {
+  return (
+    <nav className="hidden md:flex gap-2.5">
+      {navItems.map(({ label, pathname, icon: Icon }) => (
+        <Link
+          key={pathname}
+          className={cn(
+            'border border-transparent px-3 py-1.5 rounded-full text-white text-sm tracking-[-0.5px] flex items-center gap-2 hover:bg-[#DEE2E6]/20 hover:border-[#DEE2E6]/20',
+            currentPathname === pathname &&
+              'border !border-white bg-[#DEE2E6]/20',
+          )}
+          to={pathname}
+        >
+          <Icon className="w-5 h-5" />
+          <span>{label}</span>
+        </Link>
+      ))}
+    </nav>
+  );
+}
