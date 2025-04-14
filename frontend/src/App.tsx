088059cf0c9a8911b3f3c8b6856c46b55294c184
daniel christopher <daniel.2005.christopher@gmail.com>
2025-04-14 13:01:09 -0300

    chore: add mimmer and zustand in project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router';
 
-import { FabProvider } from '@app/contexts/FabContext';
 import { ThemeProvider } from '@app/contexts/ThemeContext';
 import { queryClient } from '@app/lib/queryClient';
 import { Router } from '@app/Router';
@@ -10,18 +9,13 @@ import { Router } from '@app/Router';
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <FabProvider>
-        <ThemeProvider defaultTheme="light">
-          <BrowserRouter>
-            <Router />
+      <ThemeProvider defaultTheme="light">
+        <BrowserRouter>
+          <Router />
 
-            <ReactQueryDevtools
-              position="bottom"
-              buttonPosition="bottom-left"
-            />
-          </BrowserRouter>
-        </ThemeProvider>
-      </FabProvider>
+          <ReactQueryDevtools position="bottom" buttonPosition="bottom-left" />
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
